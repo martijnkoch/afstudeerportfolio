d4431e6da72f10c23ca825d55bc1ae33cdddf00c
Martijn Koch <martijn_Waterreus@hotmail.com>
2019-09-04 17:42:07 +0200

    Owl carousel fix in webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,7 @@
 let mix = require('laravel-mix');
 let build = require('./tasks/build.js');
 let tailwindcss = require('tailwindcss');
+
 require('laravel-mix-purgecss');
 
 mix.disableSuccessNotifications();
@@ -17,9 +18,9 @@ mix.webpackConfig({
         ]),
     ],
 });
-
 mix.js('source/_assets/js/main.js', 'js')
     .sourceMaps()
+    .autoload({ jquery: ['$', 'window.jQuery', 'jQuery'] })
     .sass('source/_assets/sass/main.scss', 'css/main.css')
     .options({
         processCssUrls: false,
