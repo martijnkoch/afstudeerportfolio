af992797b0740f8c2973d47a21dd7b31e3f6bfef
Martijn Koch <martijn_waterreus@hotmail.com>
2019-09-17 16:34:20 +0200

    Some margins for mobile added

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,13 +1,13 @@
 @extends('_layouts.master')
 @section('body')
-	<!-- Hero section -->
+	<!-- Leeswijzer section -->
 	<section class="hero-section overflow-hidden">
 		<div class="hero-slider owl-carousel">
 			<div class="hero-item set-bg d-flex align-items-center justify-content-center text-center leeswijzer-text" data-setbg="/assets/img/slider-bg-4.jpg">
 				<div class="container">
                     <div class="row">         
                         <h2>Leeswijzer</h2>
-                        <p>Martijn Koch heeft van september 2019 tot januari 2020 gewerkt aan zijn afstudeeropdracht bij Greenhousegroep te Eindhoven. Deze leeswijzer is een korte verder van het gehele procesverslag. Er is onderzoek gedaan naar de mogelijkheden tot adverteren met betrekking op streaming platforms. Op deze platforms wordt vooral gegamed en hier worden de eSports wedstrijden bekeken. Mochten deze termen voor u onbekend zijn, dan adviseer ik u om op de button te klikken om naar een uitlegvideo over eSports te gaan.</p>
+                        <p>Martijn Koch heeft van september 2019 tot januari 2020 gewerkt aan zijn afstudeeropdracht bij Greenhousegroep te Eindhoven. Deze leeswijzer is een korte versie van het gehele procesverslag. Er is onderzoek gedaan naar de mogelijkheden tot adverteren met betrekking op streaming platforms. Op deze platforms wordt vooral gameplay gekeken en hier worden ook eSports wedstrijden bekeken. Mochten deze termen voor u onbekend zijn, dan adviseer ik u om op de button te klikken om naar een uitlegvideo over eSports te gaan. </p>
                         <a href="#" class="site-btn btn-hover">Ga naar video<img src="/assets/img/icons/double-arrow.png" alt="#"/></a>     
                 	</div>
                 </div>
@@ -23,13 +23,14 @@
 			-->
 		</div>
 	</section>
-	<!-- Hero section end-->
-	<!-- Intro section -->
+	<!-- Leeswijzer section end-->
+
+	<!-- De Opdracht section -->
 	<section class="intro-section gradient-bg-one">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
-					<div class="intro-text-box text-box text-white">
+					<div class="intro-text-box text-box text-white colour-behind-text">
 						<h3>De Opdracht</h3>
 						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida....</p>
 					</div>
@@ -37,8 +38,9 @@
 			</div>
 		</div>
 	</section>
-	<!-- Intro section end -->
-	<!-- Intro section -->
+	<!-- De Opdracht section end -->
+
+	<!-- Wat zijn Esports section -->
 	<section class="intro-video-section set-bg d-flex align-items-end " data-setbg="/assets/img/stadium.jpg">
 		<a href="https://www.youtube.com/watch?v=gcYaKJI7JYI" class="video-play-btn video-popup"><img src="/assets/img/icons/solid-right-arrow.png" alt="#"></a>
 		<div class="container">
@@ -48,8 +50,9 @@
 			</div>
 		</div>
 	</section>
-	<!-- Intro section end -->
-	<!-- Intro section -->
+	<!-- Wat zijn Esports section end -->
+
+	<!-- Aanpak section -->
 	<section class="intro-section gradient-bg-two">
 		<div class="container">
 			<div class="row">
@@ -62,8 +65,9 @@
 			</div>
 		</div>
 	</section>
-	<!-- Intro section end -->
-	<!-- Featured section -->
+	<!-- Aanpak section end -->
+
+	<!-- Uitvoering section -->
 	<section class="featured-section">
 		<div class="featured-bg set-bg" data-setbg="/assets/img/featured-bg.jpg"></div>
 			<div class="featured-box uitvoering-section">
@@ -73,5 +77,47 @@
 			</div>
 		</div>
 	</section>
-	<!-- Featured section end-->
+	<!-- Uitvoering section end-->
+
+	<!-- Resultaat section -->
+	<section class="intro-section gradient-bg-one">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="intro-text-box text-box text-white">
+						<h3>Resultaat</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida....</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Resultaat section end -->
+
+	<!-- Reflectiesection -->
+	<section class="intro-section gradient-bg-two">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="intro-text-box text-box text-white">
+						<h3>Reflectie</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida....</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Reflectie section end -->
+
+	<!-- Bewijs section -->
+	<section class="featured-section">
+		<div class="featured-bg set-bg" data-setbg="/assets/img/featured-bg.jpg"></div>
+			<div class="featured-box uitvoering-section">
+				<h3>Bewijs</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquamet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vestibulum posuere porttitor justo id pellentesque. Proin id lacus feugiat, posuere erat sit amet, commodo ipsum. Donec pellentesque vestibulum metus...</p>
+				<a href="#" class="read-more">Read More  <img src="img/icons/double-arrow.png" alt="#"/></a>
+			</div>
+		</div>
+	</section>
+	<!-- Bewijs section end-->
 @endsection
