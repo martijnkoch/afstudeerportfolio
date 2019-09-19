1a75053aece9da31d5425d4d0cb6b7c51c267832
Martijn Koch <martijn_waterreus@hotmail.com>
2019-09-19 16:28:58 +0200

    Partials created

---
+++ source/_partials/footer.blade.php
@@ -0,0 +1,26 @@
+<div class="pt-5 pb-5 footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-xs-12 about-company text-white">
+                <h3 class="mb-2">Afstudeer Portfolio Martijn Koch</h3>
+                <p class="pr-5 text-white-50">Voor <a href="https://www.fontys.nl" target="_blank"> Fontys </a> Hogeschool ICT</p>
+                <a href="#"><i class="fa fa-facebook-square mr-1"></i></a>
+                <a href="#"><i class="fa fa-linkedin-square"></i></a>
+            </div>
+            <div class="col-lg-6 col-xs-12 links text-white">
+                <h4 class="mt-lg-0 mt-sm-3 mb-2">Links</h4>
+                <ul class="m-0 p-0">
+                <li>- <a href="/">Leeswijzer</a></li>
+                <li>- <a href="/onderzoek">Onderzoek overzicht</a></li>
+                <li>- <a href="/products">Producten overzicht</a></li>
+                <li>- <a href="/documenten">Documenten overzicht</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col copyright">
+            <p class="text-white">© 2019. All Rights Reserved.</p>
+            </div>
+        </div>
+    </div>
+</div>
